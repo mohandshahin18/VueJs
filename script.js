a227bde198c418e297ff59781fd4095601da6eea
mohandshahin18 <sha7in147@gmail.com>
2023-05-15 21:33:39 +0300

    events

--- script.js
+++ script.js
@@ -5,6 +5,22 @@ createApp({
         return {
             name: 'Mohanad' ,
             age : 23 ,
+            link : 'https://ftms.live',
+            img : 'img-1.jpg',
+            number : 0 ,
         };
-    }
+    },
+
+    methods: {
+      sayHi(time , name) {
+            return "Good" + " " + time + " "+ name;
+      },
+      
+      //events
+      testFunction(){
+        // console.log('is fired');
+        this.number ++ ;
+      },
+      
+    },
 }).mount(".app_root");
